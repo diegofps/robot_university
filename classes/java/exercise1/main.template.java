5c84e254a67b5c69b4a0781ab5ae61222ea76a57
Diego Souza <diego@wespa.com.br>
2022-03-25 11:33:13 -0300

    Adding exercise1 main template

---
+++ classes/java/exercise1/main.template.java
@@ -0,0 +1,12 @@
+'STUD.IMPORTS'
+
+class Main {
+
+  'STUD.FUNCTIONS'
+
+  public static void main(String args[]) {
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    System.out.println('STUD.SECRET_NAME'(a, b));
+  }
+}
